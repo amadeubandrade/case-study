0777ac1f11e0a02adff79b53c077224e1e0c6002
amadeubandrade <amadeubandrade@gmail.com>
2016-07-28 20:02:52 +0100

    minor change

--- trivago-case-study/PopularMovieCell.swift
+++ trivago-case-study/PopularMovieCell.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class PopularMovieCell: UITableViewCell {
 
+    //MARK: - Outlets
+    
     @IBOutlet weak var movieName: UILabel!
     @IBOutlet weak var movieYear: UILabel!
     
+    
+    //MARK: - Initialization
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
+    
+    //MARK: - Cell Configuration
+    
     func configureCell(name: String, year: String) {
+        //TODO: - Need to change this when creating movie's class
         movieName.text = name
         movieYear.text = year
     }
