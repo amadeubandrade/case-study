591b5186f3e098a6f116771e9fa98698bcc961d2
amadeubandrade <amadeubandrade@gmail.com>
2016-07-28 20:08:26 +0100

    added new file for constants

---
+++ trivago-case-study/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  trivago-case-study
+//
+//  Created by Amadeu Andrade on 28/07/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import Foundation
+
+//MARK: - KEYs
+let KEY_TRAKT = "ad005b8c117cdeee58a1bdb7089ea31386cd489b21e14b19818c91511f12a086"
