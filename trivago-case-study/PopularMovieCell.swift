f810a502ef156fbc8d2d63e38a2370218c19927c
amadeubandrade <amadeubandrade@gmail.com>
2016-07-31 02:26:01 +0100

    Added cache mechanism

--- trivago-case-study/PopularMovieCell.swift
+++ trivago-case-study/PopularMovieCell.swift
@@ -12,8 +12,11 @@ import Alamofire
 class PopularMovieCell: UITableViewCell {
     
     //MARK: - Properties
+    
     var bannerRequest: Request?
     let acceptableContentType = ["image/jpeg", "image/gif", "image/png"]
+    let cache = NSCache()
+    
 
     //MARK: - Outlets
     
@@ -39,16 +42,24 @@ class PopularMovieCell: UITableViewCell {
         if let banner = movie.bannerUrl {
             hideOutlets(banner: false, labels: true)
             movieImage.image = nil
-            bannerRequest = Alamofire.request(.GET, banner).validate(contentType: acceptableContentType).response(completionHandler: { (_: NSURLRequest?, _: NSHTTPURLResponse?, data: NSData?, error: NSError?) in
-                if error != nil {
-                    self.hideOutlets(banner: true, labels: false)
-                    self.movieName.text = movie.name
-                    self.movieYear.text = "\(movie.year)"
-                } else if let imageData = data {
-                    let img = UIImage(data: imageData)
-                    self.movieImage.image = img
-                }
-            })
+            
+            if let img = cache.objectForKey(banner) as? UIImage {
+                //Retrieve image from cache
+                self.movieImage.image = img
+            } else {
+                bannerRequest = Alamofire.request(.GET, banner).validate(contentType: acceptableContentType).response(completionHandler: { (_: NSURLRequest?, _: NSHTTPURLResponse?, data: NSData?, error: NSError?) in
+                    if error != nil {
+                        self.hideOutlets(banner: true, labels: false)
+                        self.movieName.text = movie.name
+                        self.movieYear.text = "\(movie.year)"
+                    } else if let imageData = data {
+                        let img = UIImage(data: imageData)!
+                        self.movieImage.image = img
+                        //Add image to cache
+                        self.cache.setObject(img, forKey: banner)
+                    }
+                })
+            }
         } else {
             hideOutlets(banner: true, labels: false)
             movieName.text = movie.name
