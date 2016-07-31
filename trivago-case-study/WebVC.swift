4f262436bd46e1cd6cd1827763e95aefaeb437ac
amadeubandrade <amadeubandrade@gmail.com>
2016-07-31 12:39:45 +0100

    setup web view

--- trivago-case-study/WebVC.swift
+++ trivago-case-study/WebVC.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
 class WebVC: UIViewController {
+    
+    //MARK: - Properties
+    
+    var webView: WKWebView!
+    var urlStr: String!
+    
 
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var container: UIView!
+    
+    
     //MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView = WKWebView()
+        container.addSubview(webView)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        loadRequest(urlStr)
+    }
+    
+    
+    //MARK: - Load Request
+    
+    func loadRequest(url: String) {
+        if let urlstr = NSURL(string: urlStr) {
+            let request = NSURLRequest(URL: urlstr)
+            webView.loadRequest(request)
+        }
     }
 
 
