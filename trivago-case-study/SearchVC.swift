f08ed2403d53e6037179926c1705d3e3211c46a2
amadeubandrade <amadeubandrade@gmail.com>
2016-07-31 10:30:48 +0100

    Added Search VC file

---
+++ trivago-case-study/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  trivago-case-study
+//
+//  Created by Amadeu Andrade on 31/07/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    //MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+ 
+
+}
