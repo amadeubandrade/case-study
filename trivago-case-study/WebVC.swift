1b033e34ace9aac3e416e35e7e7f26490b3c250a
amadeubandrade <amadeubandrade@gmail.com>
2016-07-31 12:30:40 +0100

    Added web view VC

---
+++ trivago-case-study/WebVC.swift
@@ -0,0 +1,20 @@
+//
+//  WebVC.swift
+//  trivago-case-study
+//
+//  Created by Amadeu Andrade on 31/07/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class WebVC: UIViewController {
+
+    //MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
